643254a4132f08c703e8a1d4f453029a0e47d334
Michaelstanden <michaelstanden@hotmail.com>
2020-12-11 00:58:44 +0000

    added date picker to only be able to selct current date of the day user is adding recipe

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({ edge: "right"});
-  });
+    $("select").formSelect();
+    // Disable selecting any date other then todays one for a input field posted date
+    var dateToday = new Date();
+    $("#posted_date").datepicker({
+        minDate: dateToday,
+        maxDate: dateToday,
+        showClearBtn: true
+    });
+});
